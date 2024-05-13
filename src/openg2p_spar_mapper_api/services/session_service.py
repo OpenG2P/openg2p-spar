945f06473cce079fae3c79ad9040bbd60348d90e
PSNAppZ <thepsnarayanan@gmail.com>
2024-05-13 17:28:42 +0530

    Revert code

--- src/openg2p_spar_mapper_api/services/session_service.py
+++ src/openg2p_spar_mapper_api/services/session_service.py
@@ -1,16 +1,9 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
-
-from openg2p_fastapi_common.service import BaseService
 from openg2p_fastapi_common.context import dbengine
+from openg2p_fastapi_common.service import BaseService
+from sqlalchemy.ext.asyncio import async_sessionmaker
 
 
 class SessionInitializer(BaseService):
-
-    def __init__(self):
-        super().__init__("SessionInitializer")
-        self.session_maker = None
-
-    async def retrieve_session(self) -> AsyncSession:
-        if not self.session_maker:
-            session_maker = async_sessionmaker(dbengine.get(), expire_on_commit=False)
+    async def retrieve_session(self):
+        session_maker = async_sessionmaker(dbengine.get(), expire_on_commit=False)
         return session_maker
