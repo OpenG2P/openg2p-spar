1abdd640445b44cba2545a0e626a17fc71105b17
PSNAppZ <thepsnarayanan@gmail.com>
2024-06-19 15:18:59 +0530

    Unit Test: Async Mapper Link API Security Signature Validation

--- tests/test_async_mapper_controller.py
+++ tests/test_async_mapper_controller.py
@@ -11,6 +11,7 @@ from openg2p_g2pconnect_common_lib.schemas import (
     AsyncResponseMessage,
     RequestHeader,
     StatusEnum,
+    SecurityErrorCodes,
 )
 from openg2p_g2pconnect_mapper_lib.schemas import (
     LinkRequest,
@@ -39,6 +40,18 @@ from openg2p_g2pconnect_mapper_lib.schemas.update import (
 from openg2p_spar_mapper_api.controllers.async_mapper_controller import (
     AsyncMapperController,
 )
+from openg2p_spar_mapper_api.services import (
+    RequestValidationException,
+)
+
+
+# Setup side effect for validate_signature to raise RequestValidationException
+def mock_validate_signature(is_signature_valid):
+    if not is_signature_valid:
+        raise RequestValidationException(
+            code=SecurityErrorCodes.INVALID_JWT_SIGNATURE,
+            message=SecurityErrorCodes.INVALID_JWT_SIGNATURE,
+        )
 
 
 @pytest.mark.asyncio
@@ -68,6 +81,11 @@ async def test_link_async(
     mock_request_validation_get_component.return_value = (
         mock_request_validation_instance
     )
+
+    mock_request_validation_instance.validate_signature.side_effect = (
+        mock_validate_signature
+    )
+
     mock_async_response_helper_instance = MagicMock()
     expected_response = AsyncResponse(
         message=AsyncResponseMessage(
@@ -106,13 +124,96 @@ async def test_link_async(
         ),
     )
 
-    actual_response = await controller.link_async(mock_link_request)
+    actual_response = await controller.link_async(
+        mock_link_request, is_signature_valid=True
+    )
     assert (
         actual_response == expected_response
     ), "The response did not match the expected response."
     assert actual_response.message.correlation_id == "1234"
     assert actual_response.message.ack_status == AsyncAck.ACK
     assert actual_response.message.timestamp == expected_response.message.timestamp
+
+
+@pytest.mark.asyncio
+@patch(
+    "openg2p_spar_mapper_api.controllers.async_mapper_controller.AsyncResponseHelper.get_component"
+)
+@patch(
+    "openg2p_spar_mapper_api.controllers.async_mapper_controller.RequestValidation.get_component"
+)
+@patch(
+    "openg2p_spar_mapper_api.controllers.async_mapper_controller.MapperService.get_component"
+)
+async def test_link_async_invalid_signature(
+    mock_mapper_service_get_component,
+    mock_request_validation_get_component,
+    mock_async_response_helper_get_component,
+):
+    # Setup MagicMock for MapperService and RequestValidation components
+    mock_mapper_service_instance = MagicMock()
+    mock_mapper_service_instance.link = AsyncMock()
+    mock_request_validation_instance = MagicMock()
+
+    # Assign return values to the get_component mocks
+    mock_mapper_service_get_component.return_value = mock_mapper_service_instance
+    mock_request_validation_get_component.return_value = (
+        mock_request_validation_instance
+    )
+
+    mock_request_validation_instance.validate_signature.side_effect = (
+        mock_validate_signature
+    )
+
+    # Setup MagicMock for AsyncResponseHelper component
+    mock_async_response_helper_instance = MagicMock()
+    error_response = AsyncResponse(
+        message=AsyncResponseMessage(
+            correlation_id="error_correlation_id",
+            timestamp=datetime.utcnow().isoformat(),
+            ack_status="NACK",
+        )
+    )
+    mock_async_response_helper_instance.construct_error_async_response.return_value = (
+        error_response
+    )
+    mock_async_response_helper_get_component.return_value = (
+        mock_async_response_helper_instance
+    )
+
+    controller = AsyncMapperController()
+
+    mock_link_request = LinkRequest(
+        header=RequestHeader(
+            message_id="test_message_id",
+            message_ts=datetime.utcnow().isoformat(),
+            action="link",
+            sender_id="test_sender",
+            total_count=1,
+        ),
+        message=LinkRequestMessage(
+            transaction_id="test_transaction_id",
+            link_request=[
+                SingleLinkRequest(
+                    reference_id="test_ref",
+                    timestamp=datetime.utcnow(),
+                    id="test_id",
+                    fa="test_fa",
+                )
+            ],
+        ),
+    )
+
+    actual_response = await controller.link_async(
+        mock_link_request, is_signature_valid=False
+    )
+
+    assert (
+        actual_response == error_response
+    ), "The response did not match the expected error response."
+    assert actual_response.message.correlation_id == "error_correlation_id"
+    assert actual_response.message.ack_status == AsyncAck.NACK
+    assert actual_response.message.timestamp == error_response.message.timestamp
 
 
 @pytest.mark.asyncio
